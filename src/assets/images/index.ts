e4bc77eaa2f1bec28a13a45f49b5d57d4cba8615
Kiran Chhetri <chhetrikiran0999@gmail.com>
2025-07-29 10:27:01 +0545

    login page

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,3 +1,7 @@
-import Logo from "../../assets/images/logo.png";
+import Logo from "../images/logo.png";
+import UniversityLogo from "../images/university__pace-university--logo-horizontal.svg";
+import University2Logo from "../images/university__2.svg";
+import Review from "../images/Review.png";
+import Profile from "../images/image-profile.png";
 
-export {Logo};
+export { Logo, UniversityLogo, University2Logo, Review, Profile };
